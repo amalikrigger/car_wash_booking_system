3b8714240ad566f2599a23f8cd4cbb4144cf5964
amalikrigger <amalikrigger93@gmail.com>
2025-04-19 17:29:43 -0400

    Optimized class-cwb-activator.php class-cwb-deactivator.php class-cwb-loader.php

--- wp-content/plugins/car-wash-booking-plugin/includes/class-cwb-activator.php
+++ wp-content/plugins/car-wash-booking-plugin/includes/class-cwb-activator.php
@@ -1,8 +1,44 @@
 <?php
-class CWB_Activator {
-    public static function activate() {
-        include_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/database/class-database-setup.php';
-        CWB_Database_Setup::initialize_database();
-    }
+/**
+ * Plugin Activation Handler
+ *
+ * This class handles all the functionality that needs to run when
+ * the plugin is activated.
+ *
+ * @package Car_Wash_Booking
+ * @since   1.0.0
+ */
 
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Class CWB_Activator
+ *
+ * Handles plugin activation tasks like database setup
+ * 
+ * @since 1.0.0
+ */
+class CWB_Activator {
+    
+    /**
+     * Run activation tasks
+     *
+     * Sets up the database tables required by the plugin
+     * 
+     * @since 1.0.0
+     * @return void
+     */
+    public static function activate() {
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/database/class-database-setup.php';
+        CWB_Database_Setup::initialize_database();
+        
+        // Set version in options
+        update_option( 'cwb_version', '1.0.0' );
+        
+        // Flush rewrite rules to enable any custom endpoints
+        flush_rewrite_rules();
+    }
 }
--- wp-content/plugins/car-wash-booking-plugin/includes/class-cwb-deactivator.php
+++ wp-content/plugins/car-wash-booking-plugin/includes/class-cwb-deactivator.php
@@ -1,6 +1,44 @@
 <?php
-class CWB_Deactivator {
-    public static function deactivate() {
-    }
+/**
+ * Plugin Deactivation Handler
+ *
+ * This class handles all the functionality that needs to run when
+ * the plugin is deactivated.
+ *
+ * @package Car_Wash_Booking
+ * @since   1.0.0
+ */
 
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Class CWB_Deactivator
+ *
+ * Handles plugin deactivation tasks
+ * 
+ * @since 1.0.0
+ */
+class CWB_Deactivator {
+    
+    /**
+     * Run deactivation tasks
+     *
+     * Performs cleanup tasks when the plugin is deactivated
+     * 
+     * @since 1.0.0
+     * @return void
+     */
+    public static function deactivate() {
+        // Clear any scheduled events
+        wp_clear_scheduled_hook( 'cwb_daily_maintenance' );
+        
+        // Clear cached data
+        delete_transient( 'cwb_locations_data' );
+        
+        // Flush rewrite rules to remove plugin endpoints
+        flush_rewrite_rules();
+    }
 }
